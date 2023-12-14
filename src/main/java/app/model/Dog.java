37c7d0b5b82e1703e0de100f238a7123b5e791f4
proX3L <p.reznikov22@gmail.com>
2023-12-14 16:22:04 +0700

    +

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,2 +1,11 @@
-package app.model;public class Dog {
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
 }
